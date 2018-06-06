24394799fc25626fb1766fd55839b63dd6c9c287
goran <goranaviani@gmail.com>
2018-06-06 10:38:23 +0200

    If item not found in inventory add it to inventory.

--- calculation.py
+++ calculation.py
@@ -1,17 +1,27 @@
 def add(userInput,inventory):
+    #format: A,item name, amount, price
+    isFound = False
 
     for oneDict in inventory:
+        #If item is found make calculaton to get value of one item and add new stock to inventory stock.
         if userInput[1] == oneDict["NAME"]:
+            isFound = True
             #Total value of current item + Total value of added stock of current item - total number of stock item
             oneDict["VALUE"] = (float(oneDict["VALUE"] * oneDict["NUMBER"]) + (float(userInput[2]) * float(userInput[3]))) / (oneDict["NUMBER"] + int(userInput[2]))
-            print("{}".format(oneDict["VALUE"]))
             oneDict["NUMBER"] += int(userInput[2])
-            print("{}".format(oneDict["NUMBER"]))
+
+    #If item not found in inventory add it to inventory
+    if isFound == False:
+        newItem = {}
+        newItem["NAME"] = userInput[1]
+        newItem["NUMBER"] = userInput[2]
+        newItem["VALUE"] = userInput[3]
+        inventory.append(newItem)
 
     return inventory
 
 def sell(userInput,sum):
-    sum-=int(userInput[1:])
+    sum -= int(userInput[1:])
     return sum
 
 
