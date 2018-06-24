308778d4e5a27f6c873c55dff604220f5066dc94
goran <goranaviani@gmail.com>
2018-06-24 10:33:10 +0200

    Unit tests

--- test_calculation.py
+++ test_calculation.py
@@ -12,13 +12,16 @@ class AddingTestCase(unittest.TestCase):
     def test_adding_item_to_inventory(self):
         inventory = calculation.adding_item_to_inventory(['A', 'ITEM3', '3', '10'], [{'NAME': 'ITEM1', 'NUMBER': 11, 'VALUE': 50}, {'NAME': 'ITEM2', 'NUMBER': 2, 'VALUE': 4.3}])
         self.assertEqual(inventory, [{'NAME': 'ITEM1', 'NUMBER': 11, 'VALUE': 50}, {'NAME': 'ITEM2', 'NUMBER': 2, 'VALUE': 4.3}, {'NAME': 'ITEM3', 'NUMBER': 3, 'VALUE': 10.0}])
-"""
-def adding_item_to_inventory(userInput, inventory):
-    newItem = {}
-    newItem["NAME"] = userInput[1]
-    newItem["NUMBER"] = int(userInput[2])
-    newItem["VALUE"] = float(userInput[3])
-    inventory.append(newItem)
 
-    return inventory
-"""
+
+class SellingTestCase(unittest.TestCase):
+
+    def test_selling_item_still_on_lager(self):
+        oneDictNumber , oneDictValue = calculation.selling_item_still_on_lager({'NAME': 'ITEM1', 'NUMBER': 11, 'VALUE': 50}, 2)
+        self.assertEqual(oneDictNumber, 9)
+        self.assertEqual(oneDictValue, 50)
+
+
+
+
+
